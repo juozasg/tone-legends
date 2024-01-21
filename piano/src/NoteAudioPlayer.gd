1fc32eda1c68c67de56f07655eabe7f607b06d6e
Juozas Gaigalas <juozasgaigalas@gmail.com>
2024-01-21 13:11:37 +0200

    assets

--- piano/src/NoteAudioPlayer.gd
+++ piano/src/NoteAudioPlayer.gd
@@ -8,7 +8,7 @@ func _pitch_scale(c4_semitones:int) -> float:
 func play_oneshot(c4_semitones: int):
 	var audio := AudioStreamPlayer.new()
 	add_child(audio)
-	audio.stream = preload("res://piano/C4.wav")
+	audio.stream = preload("res://piano/audio/C4.wav")
 	audio.pitch_scale = _pitch_scale(c4_semitones)
 	audio.play()
 	await get_tree().create_timer(8.0).timeout
